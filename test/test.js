9e78b54a751d7920c290468cb3397b3b7117524f
Jesus Rodriguez <rpsoft@gmail.com>
2018-08-26 17:34:19 +0100

    :add: added second example to the test.

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ const tap = require('tap')
 const should = require('should')
 
 const {Observable, interval, timer} = require('rxjs');
-const {debounceTime, bufferWhen, share} = require('rxjs/operators');
+const {debounceTime, bufferWhen, share, bufferToggle, filter} = require('rxjs/operators');
 
 const {promisify} = require('util')
 const setTimerPromise = promisify(setTimeout)
@@ -11,19 +11,20 @@ tap.jobs = 6
 
 
 describe('Example:', () => {
-  context('Group burst of data that have a text to start yielding and time interval in between data to stop.', () => {
+  context('Group burst of data by idle time interval in between.', () => {
     // Stream initialization values.
     let dataStream = ['abcd', 'efg', 'hij']
     let timelapseDataStream = [10, 20, 10]
 
     // Generator that will create an stream.
-    // Each
+    // Each yield is a promise that will resolve with some data.
     function* stream(data, timelapse) {
       for (let index in data) {
         yield setTimerPromise(timelapse[index]).then(() => data[index])
       }
     }
 
+    // timelapse between data and the expected groups to end the test.
     async function groupByTimelapse(timelapse, expectedGroups) {
       let stdoutBufferNext
       let endTest
@@ -69,7 +70,7 @@ describe('Example:', () => {
       for await (const x of stream(dataStream, timelapseDataStream)) {
         stdoutBufferNext(x)
       }
-      // Await until last group is received
+      // Await until last group is received and processed by the observable.
       await end
       // Close test length checker observable.
       endUnfinishedTest.unsubscribe()
@@ -87,6 +88,88 @@ describe('Example:', () => {
     it('get 1 groups', async () => {
       const result = await groupByTimelapse(25, 1)
       result.should.containDeepOrdered([dataStream], 'It should be equal!')
+    })
+  })
+
+  context('Group burst of data that have a text to start yielding and idle time interval to return the data group.', () => {
+    // Stream initialization values.
+    let dataStream = ['abcd', 'efg', 'hij']
+    let timelapseDataStream = [10, 20, 10]
+
+    // Generator that will create an stream.
+    // Each yield is a promise that will resolve with some data.
+    function* stream(data, timelapse) {
+      for (let index in data) {
+        yield setTimerPromise(timelapse[index]).then(() => data[index])
+      }
+    }
+
+    // timelapse between data and the expected groups to end the test.
+    async function groupByTimelapse(timelapse, expectedGroups) {
+      let stdoutBufferNext
+      let endTest
+      // Create a promise and export the resolve function to 'endTest' var.
+      let end = new Promise(resolve => endTest = resolve)
+      // Will store the groups from the stream
+      let result = []
+
+      // Observable timer that in case of long time without ending it will rise an error.
+      const endUnfinishedTest = timer(100).subscribe(
+        ()=>{
+          console.error('unfinished test! unfinished Result value: ' + result)
+          endTest()
+        },
+      )
+
+      // Create an observable and export the next function to 'stdoutBufferNext' var.
+      // We need to specify the 'share' option at pipe in order to use in multiple observables.
+      const stdoutBuffer = Observable.create(observer => {
+          stdoutBufferNext = (data) => observer.next(data);
+        }).pipe(share())
+
+      // Observable that generate a signal when the observable stream has a provided text
+      const emitWhenFindText = stdoutBuffer
+        .pipe(filter(burst => burst.includes('ab')))
+
+
+      // Observable that generate a signal when the observable stream is silent for 'timelapse' time
+      const emitAfterTimeInactive = stdoutBuffer
+        .pipe(debounceTime(timelapse))
+
+      // Observable that group data from observable.
+      // It will use the signal of the emitAfterTimeInactive as trigger
+      const bufferBurstOfData = stdoutBuffer
+        .pipe(bufferToggle(emitWhenFindText, () => emitAfterTimeInactive))
+
+      // We create a subscription to 'bufferBurstOfData'
+      const observerBurstOfData = bufferBurstOfData.subscribe(x => {
+        // Add elements to the result
+        result.push(x)
+        console.log(x, result.length, expectedGroups)
+
+        // Check timer End test?
+        if (result.length == expectedGroups) {
+          endTest(true)
+        }
+      });
+      let dif = 0
+      let datenow
+      for await (const x of stream(dataStream, timelapseDataStream)) {
+        datenow = Date.now()
+        console.log(datenow - dif)
+        dif = datenow
+        stdoutBufferNext(x)
+      }
+      // Await until last group is received and processed by the observable.
+      await end
+      // Close test length checker observable.
+      endUnfinishedTest.unsubscribe()
+      return result
+    }
+
+    it('get 3 groups', async () => {
+      const result = await groupByTimelapse(5, 1)
+      result.should.containDeepOrdered([[ 'abcd', 'efg' ]], 'It should be equal!')
     })
   })
 })
